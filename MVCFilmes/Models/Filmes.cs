19c2b60a0aa181b13dd8835f1219df8c850e8c0e
Felipe Januário Domingues <felipej.domingues@gmail.com>
2023-03-02 22:18:22 -0300

    MVC do 0 ao pro

---
+++ MVCFilmes/Models/Filmes.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCFilmes.Models
+{
+    public class Filmes
+    {
+        public int ID { get; set; } 
+
+        [MaxLength(100)]
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        public string Titulo { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Display(Name = "Data de Lançamento")]
+        [DataType(DataType.Date)]
+        public DateTime DataLancamento { get; set; }
+
+        [
+            Required(ErrorMessage = "Este campo é obrigatório"), 
+            StringLength(10),
+            RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "Dados inválidos")
+        ]
+        public string Genero { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Este campo é obrigatório")]
+        [Display(Name = "Preço")] 
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Preco { get; set; }
+
+        [Range(0,5, ErrorMessage = "Somente valores de 0 a 5")]
+        public int? Pontos { get; set; }
+        
+    }
+}
